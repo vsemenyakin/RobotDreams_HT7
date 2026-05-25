233bfb671a619baaf8e1b5c4486dd8a432e79c36
Volodymyr Semenyakin <volodymyr.semenyakin@fracturedbyte.com>
2026-05-25 16:45:29 +0300

    - Removed currently not needed "getArraySize()" function

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -12,12 +12,6 @@
 	std::cout << #M_FieldName ": " << M_FieldName << std::endl;
 #endif
 
-template<typename Type, size_t Size>
-constexpr size_t getArraySize(const Type (&array)[Size])
-{
-	return Size;
-}
-
 bool equals(const float value1, const float value2, const float precision = 0.0001f)
 {
 	return std::abs(value1 - value2) < precision;
