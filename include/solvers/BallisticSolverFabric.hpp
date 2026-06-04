be0114040304cda54ccace1a949f11d0df982d4c
Volodymyr Semenyakin <volodymyr.semenyakin@fracturedbyte.com>
2026-06-04 15:46:37 +0300

    - Added initial implementation of the TableBallisticSolver - Changed "createSolver()" fabric function for using std::any instead of variadic template

--- include/solvers/BallisticSolverFabric.hpp
+++ include/solvers/BallisticSolverFabric.hpp
@@ -1,23 +1,29 @@
 ﻿#pragma once
 
 #include "solvers/SimpleBallisticSolver.hpp"
+#include "solvers/TableBallisticSolver.hpp"
+
+#include <any>
 
 enum class SolverType
 {
-	ANALYTICAL
+	ANALYTICAL,
+	TABLE
 };
- 
-template<typename ... Args>
-std::unique_ptr<IBallisticSolver> createSolver(SolverType type, Args&& ... args) {
+
+std::unique_ptr<IBallisticSolver> createSolver(SolverType type, std::any firstArg = {}) {
 	switch(type) {
 		case SolverType::ANALYTICAL:
 			return createSimpleBallisticSolver();
 		
+		case SolverType::TABLE:
+		{
+			const auto tableFileName = std::any_cast<std::string>(firstArg);
+			return createTableBallisticSolver(tableFileName);
+		}
+
 		default:
 			assert(false && "Unknown solver type");
 			return nullptr;
 	}
 }
-
-
-
---
+++ include/solvers/TableBallisticSolver.hpp
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "interfaces/IBallisticSolver.hpp"
+
+#include <memory>
+
+std::unique_ptr<IBallisticSolver> createTableBallisticSolver(const std::string& tableFileName);
